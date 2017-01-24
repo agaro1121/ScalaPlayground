9f7cc047597a24b4b7a556cd3377b8e6aeb4dcce
Anthony Garo <agaro1121@gmail.com>
2017-01-23 23:27:52 -0500

    _ => 6.4

--- src/main/scala/functional/programming/in/scala/chapter6/RNG.scala
+++ src/main/scala/functional/programming/in/scala/chapter6/RNG.scala
@@ -4,12 +4,64 @@ trait RNG {
   def nextInt: (Int, RNG)
 }
 object RNG {
+
+  /*
+  * 6.1
+  * */
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     rng.nextInt match {
       case next@(n, _) if n >= 0 && n <= Int.MaxValue ⇒ next
       case (n, rng2) if n == Int.MinValue || n < 0 ⇒ nonNegativeInt(rng2)
     }
   }
+
+  /*
+  * 6.2
+  * */
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, rng2) = nonNegativeInt(rng)
+    (1/n.toDouble, rng2)
+  }
+
+  /*
+  * 6.3
+  * */
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (n1, rng2) = nonNegativeInt(rng)
+    val (d1, rng3) = double(rng2)
+    ((n1, d1), rng3)
+  }
+
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((n,d), rng2) = intDouble(rng)
+    ((d, n), rng2)
+
+  }
+
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    ((d1, d2, d3), rng4)
+  }
+
+  /*
+  * 6.4
+  * */
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def loop(innerCount: Int, acc: (List[Int], RNG)): (List[Int], RNG) = {
+      if(innerCount == 0) acc
+      else {
+        val (xs, r) = acc
+        val (x, r2) = nonNegativeInt(r)
+        loop(innerCount - 1, (x::xs, r2))
+      }
+    }
+
+    loop(count, (Nil, rng))
+  }
+
+
 }
 
 case class SimpleRNG(seed: Long) extends RNG {
--- src/main/scala/functional/programming/in/scala/chapter6/RNGTest.scala
+++ src/main/scala/functional/programming/in/scala/chapter6/RNGTest.scala
@@ -15,6 +15,19 @@ class RNGTest extends WordSpec with Matchers {
       rng2 shouldBe SimpleRNG(126652698007966L)
     }
 
+    "implement double" in {
+      val rng = SimpleRNG(Int.MaxValue)
+      val (n, rng2) = RNG.double(rng)
+
+      n shouldBe 5.174465371378937E-10
+      rng2 shouldBe SimpleRNG(126652698007966L)
+    }
+
+    "implement intDouble" in Pending
+    "implement doubleInt" in Pending
+    "implement double3"   in Pending
+    "implement ints"      in Pending
+
   }
 
 }
