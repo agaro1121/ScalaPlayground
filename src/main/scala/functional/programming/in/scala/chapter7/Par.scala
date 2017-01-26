0ddfd1fb1126858ce3bf6095922de1617dbabbee
Anthony Garo <agaro1121@gmail.com>
2017-01-26 14:19:17 -0500

    finishes up till 7.4.4

---
+++ src/main/scala/functional/programming/in/scala/chapter7/Par.scala
@@ -0,0 +1,95 @@
+package functional.programming.in.scala.chapter7
+
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
+//trait Par[A] {}
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (_: ExecutorService) ⇒ UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def isCancelled: Boolean = false
+    override def isDone: Boolean = true
+    override def get(timeout: Long, unit: TimeUnit): A = get
+    override def cancel(mayInterruptIfRunning: Boolean): Boolean = false
+  }
+
+  /*
+  * 7.1
+  * map2 - signature
+  * */
+  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) ⇒ C): Par[C] =
+    (es: ExecutorService) ⇒ {
+      val af = pa(es)
+      val bf = pb(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def map2WithTimeout[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) ⇒ C): Par[C] =
+    (es: ExecutorService) ⇒ {
+      val (af, time) = timeTaken(pa(es))
+      val bf = pb(es)
+      UnitFuture(f(af, bf.get(time, TimeUnit.MILLISECONDS)))
+    }
+
+  private def timeTaken[A](future: ⇒ Future[A]): (A, Long) = {
+    val startTime = System.currentTimeMillis()
+    val result = future.get()
+    val endTime = System.currentTimeMillis()
+    val timeTaken = endTime - startTime
+    (result, timeTaken)
+  }
+
+  def run[A](a: Par[A])(s: ExecutorService): Future[A] = a(s)
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es ⇒ es.submit(new Callable[A] { //blocks
+      override def call: A = a(es).get
+    })
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  /*
+  * 7.4
+  * */
+  def asyncF[A, B](f: A => B): A => Par[B] =
+    (a: A) ⇒ lazyUnit(f(a))
+
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def sortPar(parList: Par[List[Int]]) = map(parList)(_.sorted)
+
+  /*
+  * 7.5
+  * */
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] =
+    ps.foldRight(unit(Nil: List[A])) { (elem, acc) ⇒ map2(elem, acc)(_ :: _) }
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] =
+    fork {
+      val fbs: List[Par[B]] = ps.map(asyncF(f))
+      sequence(fbs)
+    }
+
+  /*
+  * 7.6
+  * */
+  def parFilter[A](as: List[A])(f: A => Boolean): Par[List[A]] = {
+    /*as.foldRight(unit(Nil: List[A])) {
+      (elem, acc) ⇒
+        if(f(elem)) map(acc)(elem :: _)
+        else acc
+    }*/
+    val pars: List[Par[List[A]]] =
+      as.map(asyncF((a: A) => if (f(a)) List(a) else List()))
+    map(sequence(pars))(_.flatten)
+  }
+
+  def equal[A](e: ExecutorService)(p: Par[A], p2: Par[A]): Boolean = p(e).get == p2(e).get
+
+  //does computation when needed in main thread
+  def delay[A](fa: => Par[A]): Par[A] = es => fa(es)
+
+}
+
