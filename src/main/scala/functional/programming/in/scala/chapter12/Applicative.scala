728a13d05d9983b827fb09f25d2cee5a6faeec79
Anthony Garo <agaro1121@gmail.com>
2017-03-17 00:08:07 -0400

    12.2 && 12.3

--- src/main/scala/functional/programming/in/scala/chapter12/Applicative.scala
+++ src/main/scala/functional/programming/in/scala/chapter12/Applicative.scala
@@ -7,12 +7,23 @@ import scala.language.higherKinds
 trait Applicative[F[_]] extends Functor[F] {
 
   //primitives
-  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C]
-  def unit[A](value: => A): F[A]
+  def unit[A](a: => A): F[A]
 
   //derived
-  def map[A, B](fa: F[A])(f: A => B): F[B] =
-    map2(fa, unit(()))((a, _) => f(a))
+  //  def map[A, B](fa: F[A])(f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
+
+  /*
+  * 12.2
+  * */
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] =
+    //map2(fab, fa)(_(_))
+    map2[A, A => B, B](fa, fab)((a, f) => f(a))
+
+  def map[A, B](fa: F[A])(f: A => B): F[B] = apply[A, B](unit(f))(fa)
+
+  def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+//    apply[B, C](map(fa)(f.curried))(fb)
+  apply(apply(unit(f.curried))(fa))(fb)
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
@@ -21,13 +32,26 @@ trait Applicative[F[_]] extends Functor[F] {
   * 12.1
   * */
   def sequence[A](fas: List[F[A]]): F[List[A]] =
-//    traverse(fas)(fa => fa)
-    fas.foldRight(unit(Nil: List[A]))((e, acc) => map2(e,acc)(_ :: _))
+    //    traverse(fas)(fa => fa)
+    fas.foldRight(unit(Nil: List[A]))((e, acc) => map2(e, acc)(_ :: _))
 
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] =
     sequence(List.fill(n)(fa))
 
-  def product[A,B](fa: F[A], fb: F[B]): F[(A,B)] =
-    map2(fa, fb)((_,_))
+  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((_, _))
+
+  /*
+  * 12.4
+  * */
+  def map3[A,B,C,D](fa: F[A], fb: F[B],
+                    fc: F[C])(f: (A, B, C) => D): F[D] =
+//    apply[C,D](map2(fa, fb)((a,b) => f.curried(a)(b)))(fc)
+    apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
+
+  def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C],
+                      fd: F[D])(f: (A, B, C, D) => E): F[E] =
+//    apply[D,E](map3(fa,fb,fc)((a,b,c) => f.curried(a)(b)(c)))(fd)
+    apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
 
 }
