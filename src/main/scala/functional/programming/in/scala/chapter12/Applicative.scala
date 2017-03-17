3b9a38c234b4d0b49479b67d0bd111d7dc67a4d8
Anthony Garo <agaro1121@gmail.com>
2017-03-17 13:38:14 -0400

    12.5

--- src/main/scala/functional/programming/in/scala/chapter12/Applicative.scala
+++ src/main/scala/functional/programming/in/scala/chapter12/Applicative.scala
@@ -3,6 +3,7 @@ package functional.programming.in.scala.chapter12
 import free.Functor
 
 import scala.language.higherKinds
+import scala.util.Either.RightProjection
 
 trait Applicative[F[_]] extends Functor[F] {
 
@@ -22,8 +23,8 @@ trait Applicative[F[_]] extends Functor[F] {
   def map[A, B](fa: F[A])(f: A => B): F[B] = apply[A, B](unit(f))(fa)
 
   def map2[A, B, C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
-//    apply[B, C](map(fa)(f.curried))(fb)
-  apply(apply(unit(f.curried))(fa))(fb)
+    //    apply[B, C](map(fa)(f.curried))(fb)
+    apply(apply(unit(f.curried))(fa))(fb)
 
   def traverse[A, B](as: List[A])(f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
@@ -44,14 +45,41 @@ trait Applicative[F[_]] extends Functor[F] {
   /*
   * 12.4
   * */
-  def map3[A,B,C,D](fa: F[A], fb: F[B],
-                    fc: F[C])(f: (A, B, C) => D): F[D] =
-//    apply[C,D](map2(fa, fb)((a,b) => f.curried(a)(b)))(fc)
+  def map3[A, B, C, D](fa: F[A], fb: F[B],
+    fc: F[C])(f: (A, B, C) => D): F[D] =
+    //    apply[C,D](map2(fa, fb)((a,b) => f.curried(a)(b)))(fc)
     apply(apply(apply(unit(f.curried))(fa))(fb))(fc)
 
-  def map4[A,B,C,D,E](fa: F[A], fb: F[B], fc: F[C],
-                      fd: F[D])(f: (A, B, C, D) => E): F[E] =
-//    apply[D,E](map3(fa,fb,fc)((a,b,c) => f.curried(a)(b)(c)))(fd)
+  def map4[A, B, C, D, E](fa: F[A], fb: F[B], fc: F[C],
+    fd: F[D])(f: (A, B, C, D) => E): F[E] =
+    //    apply[D,E](map3(fa,fb,fc)((a,b,c) => f.curried(a)(b)(c)))(fd)
     apply(apply(apply(apply(unit(f.curried))(fa))(fb))(fc))(fd)
+
+}
+
+object Applicative {
+
+  val streamApplicative = new Applicative[Stream] {
+    def unit[A](a: => A): Stream[A] =
+      Stream.continually(a)
+
+    override def map2[A, B, C](a: Stream[A], b: Stream[B])(f: (A, B) => C): Stream[C] =
+      a zip b map f.tupled
+
+  }
+
+  import functional.programming.in.scala.chapter11._, Monad._
+
+  def eitherMonad[E]: Monad[({ type f[x] = Either[E, x] })#f] = new Monad[({ type f[x] = Either[E, x] })#f] {
+
+    override def flatMap[A, B](ma: Either[E, A])(f: A ⇒ Either[E, B]): Either[E, B] = {
+      ma match {
+        case Left(e) ⇒ Left(e)
+        case Right(v) ⇒ f(v)
+      }
+    }
+
+    override def unit[A](value: A) = Right(value)
+  }
 
 }
